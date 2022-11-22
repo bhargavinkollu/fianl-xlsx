1042f67343125b80470863cc77686b211eb37330
Anurag Pandey <anurag.pandey17@gmail.com>
2022-11-22 19:32:44 +0530

    pagination

--- client/src/Components/Filtershg.js
+++ client/src/Components/Filtershg.js
@@ -8,6 +8,7 @@ import * as FileSaver from "file-saver";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../action/useraction";
 import { LOader } from "./LOader";
+import { Pagination } from "./Pagination";
 export const Filtershg = () => {
   const [data, setData] = useState([]);
   const { slf, district, ulb, tlfname, years } = useParams();
@@ -88,21 +89,18 @@ export const Filtershg = () => {
     }
     return financial_year;
   }
+  let datas;
   const fmap = () => {
-    let datas;
     if (notuplod === true) {
-      datas = filterdata[0];
+      datas = currentRecords[0];
     } else if (notuplod === false) {
-      datas = filterdata;
+      datas = currentRecords;
     } else if (uploadblank === true) {
-      datas = filterdata;
+      datas = currentRecords;
     }
 
-    console.log(uploadblank);
     try {
       const ffmap = datas.map((row, indexs) => {
-        console.log(row["SHG ID"]);
-        console.log(indexs)
         return (
           // {Object}.key(datas),
           <tr>
@@ -112,7 +110,6 @@ export const Filtershg = () => {
                 return item !== "_id";
               })
               .map((key, index) => {
-                
                 // console.log(row["SHG ID"]);
                 const rooo = Object.keys(datas).filter((item, index) => {
                   // console. log(item);
@@ -209,116 +206,141 @@ export const Filtershg = () => {
             console.log(filterdata))
       );
   };
-
+  //pagination
+  console.log(filterdata);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recordsPerPage] = useState(10);
+    const indexOfLastRecord = currentPage * recordsPerPage;
+      const nPages = Math.ceil(filterdata.length / recordsPerPage);
+      const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  
+      let currentRecords = filterdata.slice(indexOfFirstRecord, indexOfLastRecord);
+      console.log(nPages);
+      console.log(filterdata.slice(indexOfFirstRecord, indexOfLastRecord));
+    
+   
+  // if (datas.length >= 10) {
+  // }
+  //pagination
   return (
-    <div className="viewlisttop">
-      <div className="viewlistboarder">
-        <SideNavigation />
-        <Header />
-        <div className="AddFlex">
-          <div style={{ width: "70%", marginLeft: "30%" }}>
-            <div style={{ width: "100%" }}>
-              <button className="btn" onClick={downloadExcel}>
-                <i class="fa fa-download" aria-hidden="true"></i>
-              </button>
+    <>
+      <div className="viewlisttop">
+        <div className="viewlistboarder">
+          <SideNavigation />
+          <Header />
+          <div className="AddFlex">
+            <div style={{ width: "70%", marginLeft: "30%" }}>
+              <div style={{ width: "100%" }}>
+                <button className="btn" onClick={downloadExcel}>
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                </button>
 
-              <div className="breadcum">
-                <ol class="breadcrumb">
-                  <Link to={`/filter`}>
+                <div className="breadcum">
+                  <ol class="breadcrumb">
+                    <Link to={`/filter`}>
+                      <li class="breadcrumb-item active" aria-current="page">
+                        Home
+                      </li>
+                    </Link>
+
                     <li class="breadcrumb-item active" aria-current="page">
-                      Home
+                      {district}
+                    </li>
+
+                    <li class="breadcrumb-item active" aria-current="page">
+                      {ulb}
+                    </li>
+
+                    <li class="breadcrumb-item active" aria-current="page">
+                      {tlfname}
+                    </li>
+
+                    <li class="breadcrumb-item active" aria-current="page">
+                      {slf}
+                    </li>
+                  </ol>
+                  <Link to={`/filter/${district}/${ulb}/${tlfname}/${years}`}>
+                    <li class="breadcrumb-item active" aria-current="page">
+                      <button className="btn btn-outline-dark">back</button>
                     </li>
                   </Link>
-
-                  <li class="breadcrumb-item active" aria-current="page">
-                    {district}
-                  </li>
-
-                  <li class="breadcrumb-item active" aria-current="page">
-                    {ulb}
-                  </li>
-
-                  <li class="breadcrumb-item active" aria-current="page">
-                    {tlfname}
-                  </li>
-
-                  <li class="breadcrumb-item active" aria-current="page">
-                    {slf}
-                  </li>
-                </ol>
-                <Link to={`/filter/${district}/${ulb}/${tlfname}/${years}`}>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    <button className="btn btn-outline-dark">back</button>
-                  </li>
-                </Link>
-              </div>
-              <label>Financial Year:</label>
-              <select
-                className="form-select-bg"
-                value={year}
-                required
-                onChange={searchdis}
-              >
-                <option selected value={getCurrentFinancialYear()}>
-                  Current year:
-                  <br />
-                  {getCurrentFinancialYear()}
-                </option>
-                <option value="2020-21">2020-21</option>
-                <option value="2021-22">2021-22</option>
-                <option value="2022-23">2022-23</option>
-                <option value="2023-24">2023-24</option>
-                <option value="2024-25">2024-25</option>
-                <option value="2025-26">2025-26</option>
-                <option value="2026-27">2026-27</option>
-                <option value="2027-28">2027-28</option>
-                <option value="2028-29">2028-29</option>
-                <option value="2029-30">2029-30</option>
-                <option value="2030-31">2030-31</option>
-              </select>
-            </div>
-
-            <button className="btn btn-primary ml-1" onClick={searchdistrict}>
-              {" "}
-              Uploaded
-            </button>
-            <button className="btn btn-primary ml-1" onClick={notuploadshgid}>
-              notuploaded
-            </button>
-            {loading ? (
-              <LOader />
-            ) : filterdata.length >= 1 ? (
-              <>
-                {newloading === true ? (
-                  <LOader />
-                ) : (
-                  <div
-                    style={{ overflow: "scroll", overflowY: "hidden" }}
-                    className="table-responsive"
-                  >
-                    <table className="table" responsive="true">
-                      <thead>
-                        <tr>
-                          <th>S.no</th>
-                          {hmap()}
-                        </tr>
-                      </thead>
-                      <tbody>{fmap()}</tbody>
-                    </table>
-                  </div>
-                )}
-              </>
-            ) : (
-              <>
-                <div>
-                  {" "}
-                  <h1>no data found</h1>
                 </div>
-              </>
-            )}
+                <label>Financial Year:</label>
+                <select
+                  className="form-select-bg"
+                  value={year}
+                  required
+                  onChange={searchdis}
+                >
+                  <option selected value={getCurrentFinancialYear()}>
+                    Current year:
+                    <br />
+                    {getCurrentFinancialYear()}
+                  </option>
+                  <option value="2020-21">2020-21</option>
+                  <option value="2021-22">2021-22</option>
+                  <option value="2022-23">2022-23</option>
+                  <option value="2023-24">2023-24</option>
+                  <option value="2024-25">2024-25</option>
+                  <option value="2025-26">2025-26</option>
+                  <option value="2026-27">2026-27</option>
+                  <option value="2027-28">2027-28</option>
+                  <option value="2028-29">2028-29</option>
+                  <option value="2029-30">2029-30</option>
+                  <option value="2030-31">2030-31</option>
+                </select>
+              </div>
+
+              <button className="btn btn-primary ml-1" onClick={searchdistrict}>
+                {" "}
+                Uploaded
+              </button>
+              <button className="btn btn-primary ml-1" onClick={notuploadshgid}>
+                notuploaded
+              </button>
+              {loading ? (
+                <LOader />
+              ) : filterdata.length >= 1 ? (
+                <>
+                  {newloading === true ? (
+                    <LOader />
+                  ) : (
+                    <div
+                      style={{ overflow: "scroll", overflowY: "hidden" }}
+                      className="table-responsive"
+                    >
+                      <table className="table" responsive="true">
+                        <thead>
+                          <tr>
+                            <th>S.no</th>
+                            {hmap()}
+                          </tr>
+                        </thead>
+                        <tbody>{fmap()}</tbody>
+                      </table>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <>
+                  <div>
+                    {" "}
+                    <h1>no data found</h1>
+                  </div>
+                </>
+              )}
+            </div>
+           {filterdata.length>=0?(
+             <Pagination
+             nPages={nPages}
+             currentPage={currentPage}
+             setCurrentPage={setCurrentPage}
+             disabledClass
+           />
+           ):("")}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
